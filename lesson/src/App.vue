da0ae2d54a5ba7eae308e0f51146988bbe79070b
System Administrator <root@Ugurs-MacBook-Air.local>
2018-12-30 15:56:52 +0300

    hot counter added..

---
+++ lesson/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+
+import Counter from "./components/Counter.vue"
+
+export default {
+  name: 'app',
+  
+
+  components:{
+    Counter
+  },
+
+  
+}
+</script>
---
+++ lesson/src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <div>
+
+        <h2>
+                The Count is {{ count}}
+        </h2>
+
+    <p>
+
+        <button @click="click += 1">Increment</button> 
+    </p>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+
+    data(){
+        return {
+            count : 0
+        };
+    }
+
+}
+</script>
+
+<style>
+
+</style>
