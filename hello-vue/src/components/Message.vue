c04bd1a5de79bf9bbc0975254a0a19f46a424796
Uğur Kazdal <developer.ugur@gmail.com>
2018-12-30 15:47:25 +0300

    webpack example

---
+++ hello-vue/src/components/Message.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="box">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.box{
+    background: #e3e3e3; padding: 18px; border:1px solid green; margin-bottom: 1em;
+}
+</style>
